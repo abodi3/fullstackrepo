a389a3fc002529c0a6720f0b967d2b46e21378f3
Albert Offei <abodi3@Alberts-MacBook-Air-1958.local>
2020-04-22 11:03:43 +0300

    1.11

--- part1app/src/index.js
+++ part1app/src/index.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (props) => {
+/*const Header = (props) => {
 
     console.log(props)
     return(
@@ -38,7 +38,7 @@ const Total = (props) => {
         )
 }
 
-/*const Part1 = (props) => {
+const Part1 = (props) => {
 
     return (
         <div>
@@ -80,7 +80,7 @@ const Part3 = (props) => {
         
         </div>
     )
-}*/
+}
 
 const App = () => {
     const course = {
@@ -109,6 +109,154 @@ const App = () => {
       <Total parts = {course.parts} />
     </div>
   )
+}*/
+
+/*
+
+const History = (allClicks, setAll) => {
+
+    
+
+    if (allClicks.length === 0) {
+        return (
+          <div>
+            No Feedback Given
+          </div>
+        )
+      } 
+
+    
+    
+      //console.log (allClicks.good)
+      //console.log (props.allClicks)
+      //console.log ('value' , props.good)
+    
+    return (
+        <div>
+            <p>Good {allClicks.good}</p>
+            <p>Neutral {allClicks.neutral}</p>
+            <p>Bad {allClicks.bad}</p>
+            
+        </div>
+        
+    )
+    
+    
+    
+}*/
+
+
+const Statistics = (props) => {
+    if (props.allClicks.length === 0) {
+        return (
+          <div>
+            No Feedback Given
+          </div>
+        )
+      } 
+    const totalFeedback = () => {
+
+        return props.good + props.neutral + props.bad
+    }
+
+    const averageFeedback = () => {
+        
+
+        return (props.good + (props.neutral*0) -props.bad)/totalFeedback()
+    }
+
+    const positiveFeedback = () =>{
+
+        return (props.good / totalFeedback() * 100)
+    }
+
+
+
+    return (
+        <table>
+            <tbody>
+            <tr>
+                <td>Good</td>
+                <td>{props.good}</td>
+            </tr>
+            <tr>
+                <td>Neutral</td>
+                <td>{props.neutral}</td>
+            </tr>
+            <tr>
+                <td>Bad</td>
+                <td>{props.bad}</td>
+            </tr>
+            <tr>
+                <td>All</td>
+                <td>{totalFeedback()}</td>
+            </tr>
+            <tr>
+                <td>Average</td>
+                <td>{averageFeedback()}</td>
+            </tr>
+            <tr>
+                <td>Positive</td>
+                <td>{positiveFeedback()} %</td>
+            </tr>
+            </tbody>
+
+        </table>
+        
+    )
+
+    
+
 }
+
+  
+const Button = ({ onClick, text }) => (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+
+
+
+  
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleGoodClick = () => {
+    setAll(allClicks.concat(good))
+    setGood(good + 1)
+  }
+
+  const handleBadClick = () => {
+    setAll(allClicks.concat(bad))
+    setBad(bad + 1)
+  }
+  
+  const handleNeutralClick = () => {
+    setAll(allClicks.concat(neutral))
+    setNeutral(neutral + 1)
+  }
+
+  return (
+    <div>
+        <h1>Give Feedback</h1>
+        <Button onClick={handleGoodClick} text='Good' />
+        <Button onClick={handleNeutralClick} text='Neutral' />
+        <Button onClick={handleBadClick} text= 'Bad' />
+        <h1>Statistics</h1>
+        <Statistics  allClicks = {allClicks} good = {good} bad = {bad} neutral = {neutral} />
+    
+       
+    </div>
+    
+  )
+  
+  }
+
 
 ReactDOM.render(<App />, document.getElementById('root'))
